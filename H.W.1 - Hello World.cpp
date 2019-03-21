d92370988c41288eeff17d31d0f3e9970e65e2fc
cosine7 <48633709+cosine7@users.noreply.github.com>
2019-03-20 20:32:35 -0400

    Add files via upload

---
+++ H.W.1 - Hello World.cpp
@@ -0,0 +1,20 @@
+/*
+Name: ChenXiao Liu
+Honor Code: I have never given nor received unauthorized aid in completing this work,
+nor have I presented someone else's work as my own.
+
+
+
+ A simple C++ program
+ */
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	cout << "Programming is great fun!\n";
+
+	system("pause");
+	return 0;
+}
