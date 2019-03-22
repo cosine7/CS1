1fb30a479baa477c9643aaf650ff69da7370f836
cosine7 <48633709+cosine7@users.noreply.github.com>
2019-03-22 10:08:13 -0400

    Update H.W.5 - Hotel Occupancy.cpp

--- H.W.5 - Hotel Occupancy.cpp
+++ H.W.5 - Hotel Occupancy.cpp
@@ -15,11 +15,11 @@ using namespace std;
 int main()
 {
 	int floor,
-		i,
-		total_room,
-		occupied_room,
-		sum_total = 0,
-		sum_occupied = 0;
+	    i,
+	    total_room,
+	    occupied_room,
+	    sum_total = 0,
+	    sum_occupied = 0;
 
 	cout << "How many floors the hotel has? ";
 	cin >> floor;
@@ -27,7 +27,7 @@ int main()
 	while (floor < 1)
 	{
 		cout << "\nInput Validation: Do not accept a value less than 1 for the number of floors."
-			 << "\nPlease enter again! ";
+		    << "\nPlease enter again! ";
 		cin >> floor;
 	}
 
@@ -48,7 +48,7 @@ int main()
 		while (total_room < 10)
 		{
 			cout << "Input validation: Do not accept a value less than 10 for the number of rooms on a floor."
-				 << "\nPlease enter again! ";
+			    << "\nPlease enter again! ";
 			cin >> total_room;
 		}
 
@@ -58,22 +58,22 @@ int main()
 		while ((occupied_room < 0) || (occupied_room > total_room))
 		{
 			cout << "Do not accept a value less than 0 and larger than total rooms.\n"
-				 << "Plese enter again! ";
+			    << "Plese enter again! ";
 			cin >> occupied_room;
 		}
-
+		
 		sum_total += total_room;
 		sum_occupied += occupied_room;
 	}
 
 	cout << setprecision(2) << fixed << showpoint
-		<< "\n----------------------------------------------"
-		<< "\nThere are " << sum_total << " rooms in this hotal.\n"
-		<< "There are " << sum_occupied << " occupied rooms.\n"
-		<< "There are " << sum_total - sum_occupied << " unoccupied rooms.\n"
-		<< "The occupancy rate for this hotel is "
-		<< static_cast<double>(sum_occupied) / sum_total * 100
-		<< "%.\n\n\n";
+	    << "\n----------------------------------------------"
+	    << "\nThere are " << sum_total << " rooms in this hotal.\n"
+	    << "There are " << sum_occupied << " occupied rooms.\n"
+	    << "There are " << sum_total - sum_occupied << " unoccupied rooms.\n"
+	    << "The occupancy rate for this hotel is "
+	    << static_cast<double>(sum_occupied) / sum_total * 100
+	    << "%.\n\n\n";
 
 
 	system("pause");
