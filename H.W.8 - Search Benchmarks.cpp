196eab1068f03e4497ec0d2863b8328c6dc7591b
cosine7 <48633709+cosine7@users.noreply.github.com>
2019-09-24 20:17:01 -0400

    Update H.W.8 - Search Benchmarks.cpp

--- H.W.8 - Search Benchmarks.cpp
+++ H.W.8 - Search Benchmarks.cpp
@@ -1,6 +1,6 @@
 /*
 Name: ChenXiao Liu
-Honor Code: I have never given nor received unauthorized aid in completing this work,
+Honor Code: I have neither given nor received unauthorized aid in completing this work,
 nor have I presented someone else's work as my own.
 
 
